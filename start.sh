0122e2a9c5aab9d158f7c3d105c385779415f83e
ismat-samadov <ismetsemedov@gmail.com>
2026-02-01 11:57:16 +0400

    init

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting Active Learning NER System..."
-echo "Installing dependencies..."
-pip install -r requirements.txt
-echo "Starting server..."
-cd api && python main_simple.py
